de06ecbb55c752a8188d75c3399072ae79013d37
Michael Rodrigues <run.michael97@gmail.com>
2023-05-13 12:59:20 -0300

    feat: ex00 done and init ex01

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,15 +6,34 @@
 /*   By: microdri <microdri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 19:55:28 by microdri          #+#    #+#             */
-/*   Updated: 2023/05/12 20:03:19 by microdri         ###   ########.fr       */
+/*   Updated: 2023/05/13 12:50:55 by microdri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cctype>
 
 int main(int argc, char **argv)
 {
+	int i;
+	size_t j;
 
-
-
+	if (argc <= 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		i = 1;
+		while (i < argc)
+		{
+			j = 0;
+			std::string str(argv[i]);
+			while (j < str.length())
+			{
+				std::cout << (char)std::toupper(str[j]);
+				j++;
+			}
+			i++;
+		}
+		std::cout << std::endl;
+	}
 }
