43206d19fcf51b9397bd9421336b77c0507ce31f
Michael Rodrigues <run.michael97@gmail.com>
2023-06-22 14:31:52 -0300

    feat: ex00 of module03 done

--- module-03/ex00/ClapTrap.hpp
+++ module-03/ex00/ClapTrap.hpp
@@ -6,8 +6,33 @@
 /*   By: microdri <microdri@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 19:12:02 by microdri          #+#    #+#             */
-/*   Updated: 2023/06/16 19:12:20 by microdri         ###   ########.fr       */
+/*   Updated: 2023/06/20 18:02:31 by microdri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
 
+#include <iostream>
+
+class ClapTrap
+{
+    public:
+        ClapTrap( void );
+        ClapTrap( std::string name );
+        ClapTrap( const ClapTrap &copy );
+        ClapTrap &operator=(const ClapTrap &other);
+        virtual ~ClapTrap();
+
+        void attack(const std::string& target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);
+
+    private:
+        std::string _name;
+        unsigned int _health;
+        unsigned int _energyPoints;
+        unsigned int _attackDamage;
+};
+
+#endif
--- module-03/ex00/main.cpp
+++ module-03/ex00/main.cpp
@@ -6,9 +6,30 @@
 /*   By: microdri <microdri@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 19:12:26 by microdri          #+#    #+#             */
-/*   Updated: 2023/06/16 19:12:27 by microdri         ###   ########.fr       */
+/*   Updated: 2023/06/22 14:29:01 by microdri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
 
+int main( void )
+{
+    ClapTrap instance("GOKU");
+
+    instance.attack("VEGETA");
+    instance.takeDamage(5);
+    instance.beRepaired(1);
+    instance.takeDamage(7);
+    instance.attack("1");
+    instance.attack("2");
+    instance.attack("3");
+    instance.attack("4");
+    instance.attack("5");
+    instance.attack("6");
+    instance.attack("7");
+    instance.attack("8");
+
+    instance.attack("trash");
+
+}
 
